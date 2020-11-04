34f83a7c677314c2e2f7d82220afad58f1618e88
OnyJ <julien.josselin75@hotmail.fr>
2020-11-04 15:59:00 +0100

    MarkdownInput displays text and title inputs

--- src/components/MarkdownInput.jsx
+++ src/components/MarkdownInput.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 const MarkdownInput = () => {
+  const useInputChange = () => {
+    const [input, setInput] = useState({});
+    const handleInputChange = (e) =>
+      setInput({
+        ...input,
+        [e.currentTarget.name]: e.currentTarget.value,
+      });
+    return [input, handleInputChange];
+  };
+
+  const [input, handleInputChange] = useInputChange();
+
   return (
     <>
       <h2>Markdown Input : </h2>
+      <form>
+        <h3>{input.title && "Title : " + input.title}</h3>
+        <div>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <h4>{input.text && "Text : " + input.text}</h4>
+        <div>
+          <input
+            type="text"
+            name="text"
+            placeholder="Text..."
+            onChange={handleInputChange}
+          />
+        </div>
+      </form>
     </>
   );
 };
