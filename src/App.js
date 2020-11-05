fd3bff5fb662109b4e1d42c8a0468267b8a4c40d
OnyJ <julien.josselin75@hotmail.fr>
2020-11-05 11:04:13 +0100

    Notes list displays (non-clickable)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NoteEdition from "./components/NoteEdition";
+import NotesList from "./components/NotesList";
 
 function App() {
   return (
     <div>
       <h1>Notepad</h1>
       <NoteEdition />
+      <NotesList />
     </div>
   );
 }
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,3 +1,29 @@
 import React from "react";
+import { nanoid } from "nanoid";
 
-// Notes list
+const NotesList = () => {
+  let list = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const name = localStorage.key(i);
+    if (name.includes("note-")) {
+      const stringItem = localStorage.getItem(name);
+      const jsonItem = JSON.parse(stringItem);
+      list.push(jsonItem);
+    }
+  }
+
+  return (
+    <>
+      <h2>Your Notes</h2>
+      <ul>
+        {list.map((element) => {
+          element.id = nanoid();
+          return <li key={element.id}>{element.title}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default NotesList;
